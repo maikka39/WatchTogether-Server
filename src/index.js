a4f3f43e85783c17f373d61e89c8fbc836e2159f
Maik de Kruif <maik@maik.dev>
2020-11-25 16:04:08 +0100

    Redo users

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ const { sanitize } = require("./utils/sanitize");
 
 const { PORT, ADMIN_USER } = require("./config");
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+const { Users } = require("./users");
 
 const router = require("./router");
+const { join } = require("path");
+const users = require("./users");
 
 logger.info("Starting server...");
 
@@ -22,49 +24,84 @@ app.use(router);
 app.use(cors());
 
 io.on("connection", (socket) => {
-  logger.info(
-    "New connection: %s",
-    JSON.stringify({
+  logger.info({
+    info: {
+      event: "connection",
       id: socket.id,
       ip: socket.conn.remoteAddress,
-    })
-  );
+    }
+  });
 
-  socket.on("join", ({}, joinCallback) => {
-    socket.on("enterRoom", ({ room, name }, callback) => {
-      for (let room in socket.rooms) {
-        if (room !== socket.id) {
-          socket.leave(room);
-        }
-      }
-      
-      const { error, user } = addUser({
+  socket.on("join", ({ room, name }, joinCallback) => {
+    logger.info({
+      info: {
+        event: "join",
         id: socket.id,
         name,
         room,
-      });
-
-      if (error) return callback(error);
-
-      socket.join(user.room);
-
-      io.to(user.room).emit("usersUpdated", {
-        users: getUsersInRoom(user.room),
-      });
-
-      // socket.emit('message', {
-      //   user: ADMIN_USER,
-      //   text: `Welcome ${user.name}, you're in room ${user.room}.`
-      // })
-
-      socket.broadcast.to(user.room).emit("message", {
-        user: ADMIN_USER,
-        text: `${user.name} has joined!`,
-      });
+      }
     });
 
+    let userMoved = false;
+
+    if (Users.exists(socket.id)) {
+      userMoved = true;
+
+      var { error, user } = Users.move(socket.id, room)
+
+      for (let room in socket.rooms) {
+        if (room !== socket.id && room !== user.room) {
+          socket.leave(room);
+
+          io.to(room).emit("message", {
+            user: ADMIN_USER,
+            text: `${user.name} has left.`,
+          });
+
+          io.to(room).emit("usersUpdated", {
+            users: Users.getFromRoom(room),
+          });
+        }
+      }
+
+    } else {
+      var { error, user } = Users.add(socket.id, name, room);
+    }
+
+    if (error) {
+      joinCallback(error);
+      return;
+    }
+
+    socket.join(user.room);
+
+    io.to(user.room).emit("usersUpdated", {
+      users: Users.getFromRoom(user.room),
+    });
+
+    // socket.emit('message', {
+    //   user: ADMIN_USER,
+    //   text: `Welcome ${user.name}, you're in room ${user.room}.`
+    // })
+
+    socket.broadcast.to(user.room).emit("message", {
+      user: ADMIN_USER,
+      text: `${user.name} has joined!`,
+    });
+
+    if (userMoved) {
+      joinCallback();
+      return;
+    }
+
     socket.on("message", ({ text }, callback) => {
-      const user = getUser(socket.id);
+      logger.info({
+        info: {
+          event: "message",
+          id: user.id,
+          text,
+        }
+      });
 
       text = sanitize(text);
 
@@ -73,36 +110,95 @@ io.on("connection", (socket) => {
         text,
       });
 
-      logger.info(
-        "Message: %s",
-        JSON.stringify({
-          id: user.id,
-          text,
-        })
-      );
-
       callback();
     });
 
+    socket.on("play", ({ progress }) => {
+      logger.info({
+        info: {
+          event: "play",
+          id: user.id,
+          room: user.room,
+          progress,
+        }
+      });
+
+      io.to(user.room).emit("play", {
+        progress,
+      });
+    });
+
+    socket.on("pause", ({ progress }) => {
+      logger.info({
+        info: {
+          event: "pause",
+          id: user.id,
+          room: user.room,
+          progress,
+        }
+      });
+
+      io.to(user.room).emit("pause", {
+        progress,
+      });
+    }); 
+
+    socket.on("changeVideo", ({ url }) => {
+      logger.info({
+        info: {
+          event: "changeVideo",
+          id: user.id,
+          room: user.room,
+          url,
+        }
+      });
+
+      io.to(user.room).emit("changeVideo", {
+        url,
+      });
+    });
+
+    socket.on("sync", ({ url, progress, playing }) => {
+      logger.info({
+        info: {
+          event: "sync",
+          id: user.id,
+          room: user.room,
+          url,
+          progress,
+          playing,
+        }
+      });
+
+      io.to(user.room).emit("sync", {
+        url,
+        progress,
+        playing,
+      });
+    });
+
     socket.on("disconnect", () => {
-      const user = removeUser(socket.id);
-
-      logger.info(
-        "User left: %s",
-        JSON.stringify({
+      logger.info({
+        info: {
+          event: "disconnect",
           id: socket.id,
-        })
-      );
+        }
+      });
 
-      if (user) {
-        io.to(user.room).emit("message", {
-          user: ADMIN_USER,
-          text: `${user.name} has left.`,
-        });
-        io.to(user.room).emit("usersUpdated", {
-          users: getUsersInRoom(user.room),
-        });
+      if (!user) {
+        return;
       }
+
+      Users.remove(user.id)
+
+      io.to(user.room).emit("message", {
+        user: ADMIN_USER,
+        text: `${user.name} has left.`,
+      });
+
+      io.to(user.room).emit("usersUpdated", {
+        users: Users.getFromRoom(user.room),
+      });
     });
 
     joinCallback();
--- src/users.js
+++ src/users.js
@@ -2,99 +2,78 @@ const { logger } = require("./utils/logger");
 const { ADMIN_USER } = require("./config");
 const { sanitize } = require("./utils/sanitize");
 
-const users = [];
+class User {
+  constructor(id, name, room) {
+    this.id = id;
+    this.name = name;
+    this.room = room;
+  }
+}
 
-function addUser({ id, name, room }) {
-  name = sanitize(name);
-  room = sanitize(room);
-
-  if (name === null || name === "" || room === null || room === "") {
-    return {
-      error: "Incorrect params",
-    };
+class Users {
+  constructor() {
+    this.users = [];
   }
 
-  let existingUser = getUser(id);
-  if (existingUser !== undefined) {
-    if (existingUser.name !== name) {
-      let { error } = checkName(name, room);
-      if (error) {
-        return {
-          error,
-        };
-      }
+  exists(id) {
+    return this.users.findIndex((user) => user.id === id) !== -1;
+  }
+
+  get(id) {
+    return this.users.find((user) => user.id === id)
+  }
+
+  move(id, room) {
+    let user = this.users.find((user) => user.id === id);
+
+    if (!user) return { error: "User not found" };
+
+    let { error } = this._verifyUser(id, user.name, room);
+    if (error) return { error };
+    
+    user.room = room;
+
+    return { user };
+  }
+
+  add(id, name, room) {
+    let { error } = this._verifyUser(id, name, room);
+    if (error) return { error };
+    
+    let user = new User(id, name, room);
+
+    this.users.push(user)
+
+    return { user };
+  }
+
+  remove(id) {
+    const i = this.users.findIndex((user) => user.id === id);
+
+    if (i !== -1) {
+      return this.users.splice(i, 1)[0];
+    }
+  }
+
+  getFromRoom(room) {
+    return this.users.filter((user) => user.room === room);
+  }
+
+  _verifyUser(id, name, room) {
+    if (
+      name === ADMIN_USER ||
+      this.users.find((user) => user.room === room && user.name === name && user.id !== id)
+    ) {
+      return {
+        error: "Username is taken",
+      };
     }
 
-    logger.info("User moved: %s", JSON.stringify({
-      id: id,
-      old_room: existingUser.room,
-      new_room: room,
-      old_name: existingUser.name,
-      new_name: name
-    }));
-
-    existingUser.name = name;
-    existingUser.room = room;
-
-    return {
-      user: existingUser,
-    };
+    return {}
   }
-
-  let { error } = checkName(name, room);
-  if (error) {
-    return {
-      error,
-    };
-  }
-
-  const user = {
-    id,
-    name,
-    room,
-  };
-
-  logger.info("User joined: %s", JSON.stringify(user));
-
-  users.push(user);
-
-  return {
-    user,
-  };
-}
-
-function removeUser(id) {
-  const i = users.findIndex((user) => user.id === id);
-
-  if (i !== -1) {
-    return users.splice(i, 1)[0];
-  }
-}
-
-function getUser(id) {
-  return users.find((user) => user.id === id);
-}
-
-function getUsersInRoom(room) {
-  return users.filter((user) => user.room === room);
-}
-
-function checkName(name, room) {
-  if (
-    name === ADMIN_USER ||
-    users.find((user) => user.room === room && user.name === name)
-  ) {
-    return {
-      error: "Username is taken",
-    };
-  }
-
-  return {}
 }
 
 module.exports = {
-  addUser,
-  removeUser,
-  getUser,
-  getUsersInRoom,
+  User,
+  Users: new Users(),
 };
